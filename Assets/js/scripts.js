de9506c07961d42f3a706fb2901e6dae917852c1
Jure Sulič <j.sulic@gmail.com>
2024-11-08 17:44:52 +0200

    Updated landing page, added zoom for videos

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,32 +1,58 @@
 // Wait for the DOM to load before running JavaScript
 document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.getElementById("myModal");
+    const imgModal = document.getElementById("imgModal");
+    const vidModal = document.getElementById("vidModal");
     const modalImg = document.getElementById("modal-img");
-    const captionText = document.getElementById("caption");
+    const modalVid = document.getElementById("modal-vid");
+    const captionImg = document.getElementById("captionImg");
+    const captionVid = document.getElementById("captionVid");
     const closeModal = document.querySelector(".close");
 
     let originalRect; // To store the original position of the image
 
     // Function to set modal image width based on container width
-    function setModalImageWidth() {
+    function setModalWidth() {
         const containerWidth = document.querySelector('.container').offsetWidth;
         modalImg.style.width = `${containerWidth}px`;
+        modalVid.style.width = `${containerWidth}px`;
     }
 
     // Add click event to each gallery image to open modal
     document.querySelectorAll('.gallery-img').forEach(img => {
         img.addEventListener('click', function() {
-            modal.style.display = "flex"; // Show modal
+            imgModal.style.display = "flex"; // Show modal
             modalImg.src = this.src; // Set image source
-            captionText.innerHTML = this.alt; // Set caption text
-            setModalImageWidth(); // Match modal image width with container
+            captionImg.innerHTML = this.alt; // Set caption text
+
+            const itemType = this.tagName.toLowerCase(); // Check if it's an image or video
+
+            setModalWidth(itemType); // Match modal image width with container
             
             // Store the original dimensions of the image
             originalRect = modalImg.getBoundingClientRect();
 
-            // Add mouse movement effect to modal image
             modalImg.addEventListener('mousemove', parallaxEffect);
             modalImg.addEventListener('mouseleave', resetImagePosition);
+        });
+    });
+
+    // Add click event to each gallery image to open modal
+    document.querySelectorAll('.gallery-vid').forEach(vid => {
+        vid.addEventListener('click', function() {
+            vidModal.style.display = "flex"; // Show modal
+            modalVid.src = this.src; // Set image source
+            captionVid.innerHTML = this.title; // Set caption text
+            
+            const itemType = this.tagName.toLowerCase(); // Check if it's an image or video
+
+            setModalWidth(itemType); // Match modal image width with container
+            
+            // Store the original dimensions of the image
+            originalRect = modalVid.getBoundingClientRect();
+            
+            // Add mouse movement effect to modal image
+            modalVid.addEventListener('mousemove', parallaxEffect);
+            modalVid.addEventListener('mouseleave', resetImagePosition);
         });
     });
 
@@ -44,31 +70,44 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Apply translation to create parallax effect
         modalImg.style.transform = `translate(${moveX}px, ${moveY}px) scale(${scaleFactor})`;
+        // Apply translation to create parallax effect
+        modalVid.style.transform = `translate(${moveX}px, ${moveY}px) scale(${scaleFactor})`;
     }
 
     // Reset the image position when the mouse leaves the modal image
     function resetImagePosition() {
         modalImg.style.transform = 'translate(0, 0) scale(1)'; // Reset to initial position
+        modalVid.style.transform = 'translate(0, 0) scale(1)'; // Reset to initial position
     }
 
     // Close modal and remove parallax effect on modal close
     function closeModalAndReset() {
-        modal.style.display = "none";
+        imgModal.style.display = "none";
+        vidModal.style.display = "none";
         resetImagePosition(); // Reset position
         modalImg.removeEventListener('mousemove', parallaxEffect); // Remove parallax listener
         modalImg.removeEventListener('mouseleave', resetImagePosition); // Remove reset listener
+        modalVid.removeEventListener('mousemove', parallaxEffect); // Remove parallax listener
+        modalVid.removeEventListener('mouseleave', resetImagePosition); // Remove reset listener
     }
 
     // Close modal when clicking the 'close' button
     closeModal.onclick = closeModalAndReset;
 
     // Close modal when clicking outside the image
-    modal.onclick = function(event) {
-        if (event.target === modal) {
+    imgModal.onclick = function(event) {
+        if (event.target === imgModal) {
+            closeModalAndReset();
+        }
+    }
+
+    // Close modal when clicking outside the image
+    vidModal.onclick = function(event) {
+        if (event.target === vidModal) {
             closeModalAndReset();
         }
     }
 
     // Update modal image width on window resize
-    window.addEventListener('resize', setModalImageWidth);
+    window.addEventListener('resize', setModalWidth);
 });
